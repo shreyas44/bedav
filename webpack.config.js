378a08ca311ebb4eec6f4215d1f062ad51379466
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-08-11 18:12:44 +0530

    Updated dependencies and added ESLint

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  mode: "development",
+  entry: "./bedav/pages/src/index.js",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, "bedav/pages/static/pages/js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
