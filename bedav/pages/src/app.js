88c78b63c69d051ceb86180fa472461783c3c7f3
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-08-11 18:14:32 +0530

    Created base App component with Routing and search bar component

--- bedav/pages/src/app.js
+++ bedav/pages/src/app.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+import Header from './components/header'
+import SearchBar from './components/searchBar'
+import RegisterForm from './components/register'
+import LoginForm from './components/login'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+const ContentWrapper = styled.div`
+  max-width: 1500px;
+  margin: auto;
+`
+
+const MainContainer = styled.div`
+  font-family: 'Roboto', sans-serif;
+`
 
 function App() {
-  return 
+  return (
+    <MainContainer>
+      <Router>
+        <Header />
+        <ContentWrapper>
+          <Route exact path="/"><SearchBar /></Route>
+          <Route exact path="/register"><RegisterForm /></Route>
+          <Route exact path="/login"><LoginForm /></Route>
+        </ContentWrapper>
+      </Router>
+    </MainContainer>
+  )
 }
 
 export default App
--- bedav/pages/src/components/searchBar.js
+++ bedav/pages/src/components/searchBar.js
@@ -1,9 +1,60 @@
-import react from 'react'
+import React, {useState, useRef} from 'react'
+import styled from 'styled-components'
+import MaterialSearchIcon from '@material-ui/icons/Search'
 
-function SearchBar(props) {
+let SearchIcon = ({className}) => <MaterialSearchIcon className={className} />
+
+const StyledSearchIcon = styled(SearchIcon)`
+  height: inherit !important;
+  padding: 0 0 0 20px !important;
+  font-size: 1.75rem !important;
+  transition: color 0.1s !important;
+  color: ${props => props.focused ? "#000" : "#777"};
+`
+
+const StyledDiv = styled.div`
+  width: 60%;
+  box-shadow: 7px 15px 25px 0px rgba(0,0,0,0.1);
+  margin: 25vh auto 0;
+  height: 60px;
+  border: 1px solid #FEFEFE;
+  display: flex;
+  align-content: center;
+  transition: all 0.1s;
+
+  &:focus-within {
+    height: 65px;
+    width: 61%;
+    outline: none;
+    box-shadow: 7px 15px 25px rgba(0,0,0,0.3);
+  }
+`
+
+const StyledInput = styled.input`
+  height: inherit;
+  box-sizing: border-box;
+  padding: 20px;
+  font-size: 18px;
+  border: none;
+  flex: 1;
+  font-weight: 500;
+
+  &:focus {
+    outline: none;
+    border: none;
+  }
+`
+
+function SearchBar() {
+  let [query, setQuery] = useState('')
+  let [focused, setFocus] = useState(false)
+
   return (
-    <div>
-      
-    </div>
+    <StyledDiv> 
+      <StyledSearchIcon focused={focused}/>
+      <StyledInput type="text" value={query} onChange={event => { setQuery(event.target.value) }} placeholder="Search for a hospital..." onFocus={() => { setFocus(true) }} onBlur={() => { setFocus(false) }} />
+    </StyledDiv>
   )
 }
+
+export default SearchBar
