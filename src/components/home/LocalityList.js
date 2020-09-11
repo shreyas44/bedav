fb9ed6e9d33b43ba42d76248159147500d903175
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-09-11 20:09:10 +0530

    Fixed locality row key issue

--- src/components/home/LocalityList.js
+++ src/components/home/LocalityList.js
@@ -5,7 +5,7 @@ import LocalityRow from './LocalityRow'
 function LocalityList(props) {
   const {localities} = props
   const counter = useRef(-1)
-  const items = localities.map((item) => { counter.current += 1; return <LocalityRow key={item.node.id} locality={item.node} counter={counter.current}/> } )
+  const items = localities.map(locality => { counter.current += 1; return <LocalityRow key={`${locality.node.name}-${locality.node.state}`} locality={locality.node} counter={counter.current}/> } )
 
   return <>{items}</>
 }
