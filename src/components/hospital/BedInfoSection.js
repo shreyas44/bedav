f66d2f67937dd74574a4025895ec84bcb2054b3c
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-11 05:48:35 +0530

    Shows only the data available in the hospital page

--- src/components/hospital/BedInfoSection.js
+++ src/components/hospital/BedInfoSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import data from '../extra/data'
 import BedTypeSection from './BedTypeSection'
 import HospitalHeading from './HospitalHeading'
 
@@ -23,8 +24,15 @@ const StyledHospitalHeading = styled(HospitalHeading)`
 
 function BedInfo(props) {
   const {hospital} = props
+  const key = `${hospital.locality.name.toLowerCase()}-${hospital.locality.state.toLowerCase()}`
+  const columns = data.columns[key]
   const values = {
     "General Ward": {
+      available: "N.A.",
+      occupied: "N.A.",
+      total: "N.A."
+    },
+    "General Ward with Oxygen (O2)": {
       available: "N.A.",
       occupied: "N.A.",
       total: "N.A."
@@ -43,11 +51,43 @@ function BedInfo(props) {
       available: "N.A.",
       occupied: "N.A.",
       total: "N.A."
-    }
+    },
   }
+
+  // check for columns
+  
+  if (!columns.includes("general")) {
+    delete values["General Ward"]
+  }
+
+  if (!columns.includes("oxygen")) {
+    delete values["General Ward with Oxygen (O2)"]
+  }
+
+  if (!columns.includes("icu")) {
+    delete values["Intensive Care Unit"]
+  }
+
+  if (!columns.includes("hdu")) {
+    delete values["High Dependency Unit"]
+  }
+
+  if (!columns.includes("ventilator")) {
+    delete values["Ventilators"]
+  }
+ 
+  // check if null
   
   if(hospital.generalTotal !== null && hospital.generalTotal > 0) {
     values["General Ward"] = {
+      available: hospital.generalAvailable,
+      occupied: hospital.generalOccupied,
+      total: hospital.generalTotal
+    }
+  }
+
+  if(hospital.oxygenTotal !== null && hospital.oxygenTotal > 0) {
+    values["General Ward with Oxygen (O2)"] = {
       available: hospital.generalAvailable,
       occupied: hospital.generalOccupied,
       total: hospital.generalTotal
