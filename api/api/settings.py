4f2d9a542da6cca48217c866ea8a66ee5a886b50
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-20 05:35:40 +0530

    Updated files to keep the django secret key, secert

--- api/api/settings.py
+++ api/api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=u7hf@6#%-0p+rnvexrx@#ix=fc0)$yqjsp1aa%*k$r91a*ka('
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
