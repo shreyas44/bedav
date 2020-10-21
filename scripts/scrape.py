0cd631b2537c0aafe6fd1ec5b614f62475bce65f
Shreyas Sreenivas <46835608+shreyas44@users.noreply.github.com>
2020-10-21 17:26:44 +0530

    Update scrape.py
    Update scrape.py to log the time it was last run

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime
 
 cities = [
     "bangalore",
@@ -10,4 +11,5 @@ cities = [
 while True:
     for cmd in [f"python {city}.py" for city in cities]:
         os.system(cmd)
+    print(f'Last run at {datetime.datetime.now().isoformat()}')
     time.sleep(3600)
