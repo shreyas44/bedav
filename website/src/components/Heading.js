96811c3b80dc0a8684ed5936080a4de1cef68e94
raoniveditha <nivedith@usc.edu>
2020-10-27 02:32:41 -0700

    Added metadata for Google, Facebook and Twitter.

--- website/src/components/Heading.js
+++ website/src/components/Heading.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
-
+import Helmet from "react-helmet";
 const HeadingContainer = styled.div`
   font-family: "Quicksand", sans-serif;
   float: left;
@@ -27,11 +27,35 @@ function Heading() {
   return (
     <HeadingContainer>
       <StyledLink to="/">
-        bedav
+        Bedav -
         <StyledDescription>
-          Find the hospital you need
+            Find hospital bed availability for COVID Patients across India
         </StyledDescription>
       </StyledLink>
+      <Helmet>
+        {/*<!-- HTML Meta Tags -->*/}
+        <title>Bedav - Find hospital beds for COVID Patients</title>
+        <meta name="description" content="Find hospital bed availability for COVID Patients across India. Currently available for major cities in Karnataka, Maharashtra, Andhra Pradesh."/>
+
+        {/*<!-- Google / Search Engine Tags -->*/}
+        <meta itemProp="name" content="Bedav - Find hospital beds for COVID Patients"/>
+        <meta itemProp="description" content="Find hospital bed availability for COVID Patients across India. Currently available for major cities in Karnataka, Maharashtra, Andhra Pradesh."/>
+        <meta itemProp="image" content="https://www.1-more-thing.com/wp-content/uploads/2020/04/modified.png"/>
+
+        {/*<!-- Facebook Meta Tags -->*/}
+        <meta property="og:url" content="https://bedav.org"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Bedav - Find hospital beds for COVID Patients"/>
+        <meta property="og:description" content="Find hospital bed availability for COVID Patients across India. Currently available for major cities in Karnataka, Maharashtra, Andhra Pradesh."/>
+        <meta property="og:image" content="https://www.1-more-thing.com/wp-content/uploads/2020/04/modified.png"/>
+
+        {/*}<!-- Twitter Meta Tags -->*/}
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="Bedav - Find hospital beds for COVID Patients"/>
+        <meta name="twitter:description" content="Find hospital bed availability for COVID Patients across India. Currently available for major cities in Karnataka, Maharashtra, Andhra Pradesh."/>
+        <meta name="twitter:image" content="https://www.1-more-thing.com/wp-content/uploads/2020/04/modified.png"/>
+
+      </Helmet>
     </HeadingContainer>
   )
 }
