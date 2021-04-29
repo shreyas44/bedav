e6052bbfcaa9f79bed453f2bee238c778896f56f
shreyas44 <46835608+shreyas44@users.noreply.github.com>
2021-04-29 16:04:58 +0000

    Delete old api and scripts

--- api/hospitals/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Hospital
-
-# class NewHospitalForm(forms.ModelForm):
-#   class Meta:
-#     model = Hospital
-#     fields = ['name', 'email', 'phone', 'latitude', 'longitude', 'city', 'district', 'state']
